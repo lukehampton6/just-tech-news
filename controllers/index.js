6cddf91647079952dedb8176c7d6eeef967cffe1
Luke Hampton <lukehampton6@gmail.com>
2021-07-20 16:51:34 -0400

    i dont even know

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 
-const commentRoutes = require('./comment-routes');
+const commentRoutes = require('./api/comment-routes');
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/comments', commentRoutes);
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
